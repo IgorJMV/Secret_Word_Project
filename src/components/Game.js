891d05c20f91648d29d0616d9831247e973b273f
IgorJMV <igorjmv2000@gmail.com>
2025-12-06 20:56:01 -0300

    Alternating stages of the game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,12 @@
 import "./Game.css";
 
-const Game = () => {
-  return <div>Game</div>;
+const Game = ({ verifyLetter }) => {
+  return (
+    <div>
+      <h1>Game</h1>
+      <button onClick={verifyLetter}>FInalizar Jogo</button>
+    </div>
+  );
 };
 
 export default Game;
